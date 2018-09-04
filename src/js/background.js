ba0e17346c228a1be4ae185cad80e70b3cce4ef7
lizongying <lizongying@msn.com>
2018-09-04 16:37:02 +0800

    修复http bug

--- src/js/background.js
+++ src/js/background.js
@@ -6,8 +6,7 @@ window.whatsapp = {
     content: 'StarMaker appeal to our users and the public to #StandWithKerala/Kodagu. Donate for Kerala and Kodagu to spread your love, write down your best wishes for the people who are waiting for rescue. As long as everyone give a little love, we can light up the darkest time of the country.\n' +
     '👇👇👇👇👇👇👇👇\n' +
     'https://m.starmakerstudios.com/t?promotion_id=1153&showBar=1&showNavigation=true&new=true',
-    // groupsLink: 'https://m1res-1256122840.cos.ap-mumbai.myqcloud.com/whatsapp_group/',
-    groupsLink: 'http://songbook.ushow.media/whatsapp/obtain/0/0/',
+    groupsLink: 'http://songbook.ushow.media/whatsapp/obtain/0/200/',
     groups: [],
     groupsIndex: 0,
     lastPhone: 0,
@@ -23,13 +22,41 @@ Object.keys(whatsapp).map((item) => {
 
 chrome.runtime.onMessage.addListener(
     (request, sender, sendResponse) => {
+        if (request instanceof Array) {
+            eval(request[0])(...request.slice(1));
+            return
+        }
         if (request instanceof Object) {
             Object.keys(request).map((item) => {
                 whatsapp[item] = request[item];
                 chrome.storage.sync.set({[item]: request[item]});
             });
             sendResponse({code: 0});
-        } else {
-            sendResponse({code: 0, data: whatsapp[request]});
+            return
         }
+        sendResponse({code: 0, data: whatsapp[request]});
     });
+
+const get_groups = (url) => {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+            const status = xhr.status;
+            if (status === 200) {
+                const data = xhr.responseText;
+                whatsapp.groups = data.replace(/\s/g, '').split(',');
+                if (whatsapp.groups.length) {
+                    chrome.tabs.query({'active': true, currentWindow: true}, (tabs) => {
+                        chrome.tabs.sendMessage(tabs[0].id, {groups: whatsapp.groups});
+                    });
+                } else {
+                    alert('no groups');
+                }
+            } else {
+                alert('no groups');
+            }
+        }
+    };
+    xhr.open('GET', url, true);
+    xhr.send();
+};
--- src/js/content.js
+++ src/js/content.js
@@ -134,27 +134,7 @@ $(() => {
                     return
                 }
                 const groupUrl = response.data + phone;
-                $.ajax({
-                    url: groupUrl,
-                    beforeSend: () => {
-                        $('#group-add').attr('disabled', true);
-                    },
-                    success: (data) => {
-                        groups = data.replace(/\s/g, '').split(',');
-                        if (groups.length) {
-                            chrome.runtime.sendMessage({groups: groups});
-                            setTimeout(addGroup, 0);
-                        } else {
-                            alert('no groups');
-                        }
-                    },
-                    error: () => {
-                        alert('no groups');
-                    },
-                    complete: () => {
-                        $('#group-add').attr('disabled', false);
-                    }
-                });
+                chrome.runtime.sendMessage(['get_groups', groupUrl]);
             });
         });
     };
@@ -182,6 +162,99 @@ $(() => {
     });
     setInterval(setBtn, 10);
     const sureBtnInterval = setInterval(sureBtn, 10);
+    chrome.runtime.onMessage.addListener(
+        (request, sender, sendResponse) => {
+            if (request['groups']) {
+                groups = request['groups'];
+                console.log('groups', groups);
+            }
+            setTimeout(addGroup, 0);
+        });
+
+
+    // $('#app').on("click", "[title='Group info'], ._1WBXd", function () {
+    //     //Calling function to add 'Download Contact Info' on click event opening 'Group Info' section
+    //     setTimeout(addButton, 100);
+    // });
+    //
+    // var addButton = function () {
+    //     //Add 'Download Contact Info' button to group info section
+    //     if (!$("#DownloadButton").length) {
+    //         var button_tag = document.createElement("div");
+    //         button_tag.innerHTML = '<div id="DownloadButton" class="_14oqx" role="button"><div class="DcItJ"><div class="_3WCza"><span class="_3LL06">Download Group Contacts</span></div></div></div>';
+    //
+    //         var menu_dom = document.getElementsByClassName("AfVTG")[0].children[3];
+    //         menu_dom.prepend(button_tag);
+    //
+    //         $("#DownloadButton").click(function () {
+    //             downloadInfo()
+    //         });
+    //     }
+    // }
+    //
+    // var downloadInfo = function () {
+    //     // Crawling data while scrolling the group info member list,
+    //     // storing it to array of objects and finnaly converting it to CSV and
+    //     // exporting to local directory
+    //
+    //     var body_elem = document.getElementsByTagName("BODY")[0];
+    //
+    //     var loading_elem = document.createElement("div");
+    //
+    //     loading_elem.innerHTML = '<div id="loadingElem" style="width:100%;height:100%;background-color: #2321217a;z-index: 10000;position: absolute;color: white;text-align: center;font-weight: 400;font-size: 22px;"><h2 style="position: relative;top: 50%;transform: translateY(-50%);">Downloading Contacts.....</h2></div>';
+    //     body_elem.prepend(loading_elem)
+    //
+    //     var objDiv = $("._2sNbV")[0]
+    //
+    //     objDiv.scrollTop = objDiv.scrollHeight;
+    //
+    //     var group_dict = {}
+    //
+    //     $(objDiv).scroll(function () {
+    //         var contact_obj = document.getElementsByClassName("_2sNbV")[0].getElementsByClassName("_1CRb5 _34vig")[4].lastChild.children[0].children
+    //         for (var i = 0; i < contact_obj.length; i++) {
+    //
+    //             var base_path = contact_obj[i].children[0].children[0];
+    //             var contact_number = base_path.children[1].children[0].children[0].children[0].innerText;
+    //             contact_number = contact_number.replace(/(\r\n\t|\n|\r\t)/gm,"");
+    //             if (!(contact_number in group_dict)) {
+    //                 var name_path = base_path.children[1].children[1].children[1].children[0].children[0];
+    //
+    //                 if (name_path) {
+    //                     var display_name = name_path.innerText;
+    //                 }else{
+    //                     var display_name = null;
+    //                 }
+    //
+    //                 var country_code = ''
+    //
+    //                 if(contact_number[0] == "+"){
+    //                     for(var i=0 ; i<3 ; i++){
+    //                         if (contact_number[i] != ' '){
+    //                             country_code += contact_number[i];
+    //                         }else{
+    //                             break;
+    //                         }
+    //                     }
+    //                 }
+    //
+    //                 group_dict[contact_number] = {
+    //                     "display_name":display_name,
+    //                     "country_code":country_code
+    //                 }
+    //             }
+    //         }
+    //     });
+    //
+    //     $(objDiv).animate({
+    //             scrollTop: $(objDiv).offset().top,
+    //         }, 5000
+    //         ,function () {
+    //             $("#loadingElem").remove();
+    //             var fileTitle = document.getElementsByClassName("_2sNbV")[0].getElementsByClassName("_1CRb5 _34vig")[0].children[1].children[0].children[0].children[1].innerText
+    //             exportCSVFile(group_dict, fileTitle);
+    //         });
+    // }
 });
 
 
